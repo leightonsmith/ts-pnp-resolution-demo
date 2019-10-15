9f8eceabf91b42f3358e7ec75bf2118ca75aaa9a
Leighton Smith <ljsmith@agiledigital.com.au>
2019-10-16 09:32:50 +1100

    adding ts-pnp bug demos

---
+++ src/demoFolderMasking.ts
@@ -0,0 +1,9 @@
+import { find } from 'lodash/fp'
+
+export const testCompiler: () => number | undefined = () => {
+
+  const test = [1,2,3];
+  const found = find(v => v === 1, test);
+  return found;
+
+}
---
+++ src/demoVersionSelection.ts
@@ -0,0 +1,10 @@
+import { flow, orderBy } from 'lodash/fp'
+
+export const testCompiler: () => number[] | undefined = () => {
+
+  const test = [1,2,3];
+  
+  const ordered = flow(orderBy(v => v, 'desc'))(test);
+  return ordered;
+
+}
